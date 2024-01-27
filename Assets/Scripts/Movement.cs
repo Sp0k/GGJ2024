14afaec7b613ea06a94b12f24fe37f0eb865e4e6
Anthony <anthonydaniel123@live.com>
2024-01-27 11:29:08 -0400

    Proper movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,28 +4,53 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public Transform Camera;
-    private Rigidbody rb;
+    public CharacterController controller;
+    public Transform groundCheck;
 
-    public int gravity = 2;
+    public float gravity = -9.81f;
     public int speed = 5;
     public int jumpForce = 10;
+    public float turnSmoothTime = 0.1f;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    float turnSmoothVelocity;
+    bool isGrounded;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        Physics.gravity = new Vector3(0, Physics.gravity.y * gravity, 0);
+        controller = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        transform.Translate(new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime);
+        if (isGrounded && velocity.y < 0) 
+        {
+            velocity.y = -2f;
+        }
 
         if (Input.GetKeyDown("space"))
-            rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+        {
+            velocity.y += jumpForce;
+        }
 
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
